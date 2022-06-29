59e17dc5d3da2e2fd1afc52b973dc6d549c9fdfb
Marian Stochanskyi <marian.stochanskyi@sigma.software>
2022-06-30 00:30:43 +0300

    Add fetching data for ios

--- demo_ios/demo_ios/ContentView.swift
+++ demo_ios/demo_ios/ContentView.swift
@@ -2,10 +2,32 @@ import SwiftUI
 import demo_kmm_module
 
 struct ContentView: View {
-	let greet = "Hola"
+    
+    // TODO Should be moved to some DI
+	let fetchUseCase = FetchTestDataUseCase(
+        testDataRepository: TestDataRepositoryImpl(
+            testNetworkDataSource: TestNetworkDataSource(
+                networkClient: NetworkClientImpl()
+            )
+        )
+    )
+    
+    @State var greet = ""
+    
+    func load() {
+        fetchUseCase.invoke { result, error in
+                if let result = result {
+                    self.greet = result.title
+                } else if let error = error {
+                    greet = "Error: \(error)"
+                }
+            }
+        }
 
 	var body: some View {
-		Text(greet)
+        Text(greet).onAppear() {
+            load()
+        }
 	}
 }
 
