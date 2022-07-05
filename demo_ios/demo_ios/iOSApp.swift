fe7f0ed6019d283c84d250a5fcfa74749ae7d92e
Marian <marian.stochanskyi@sigma.software>
2022-07-05 19:46:28 +0300

    Add di to ios app

--- demo_ios/demo_ios/iOSApp.swift
+++ demo_ios/demo_ios/iOSApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import demo_kmm_module
 
 @main
 struct iOSApp: App {
+    
+    init() {
+        KoinHelperKt.doInitKoin()
+    }
+    
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
